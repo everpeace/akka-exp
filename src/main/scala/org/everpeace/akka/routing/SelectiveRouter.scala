05a166c0e8f53aab6da3668b45a60c594917fe66
Shingo Omura <everpeace@gmail.com>
2011-12-17 18:43:45 +0900

    implements PollingCollector

--- src/main/scala/org/everpeace/akka/routing/SelectiveRouter.scala
+++ src/main/scala/org/everpeace/akka/routing/SelectiveRouter.scala
@@ -7,6 +7,7 @@ import akka.actor.UntypedChannel._
 import akka.event.EventHandler
 import akka.stm.TransactionalMap
 import akka.stm.atomic
+import java.util.concurrent.TimeUnit
 
 /**
  *
@@ -33,8 +34,8 @@ trait SelectiveRouter extends Dispatcher {
   }
 
   // standard select strategies is implemented by Selector trait
-  // Note: this have an argument of Any type so that
-  // message dependent selection can be implemented.
+  // Note: this has an argument of Any type
+  // so that message dependent selection can be implemented.
   protected def select: Any => Option[ActorRef]
 }
 
@@ -43,12 +44,14 @@ trait Selector {
   protected def select: Any => Option[ActorRef]
 
   //standard collection strategy is implemented by Collector
-  protected def collectedLoads: Seq[(ActorRef, Load)]
+  // Note: this has an argument of Any type
+  // so that message dependent selection can be implemented.
+  protected def collectLoads: Any => Seq[(ActorRef, Load)]
 }
 
-// loadの集合からminimumをもつActorを選択するSelector
+// 集めてきたloadの集合からminimumをもつActorを選択するSelector
 trait MinLoadSelector extends Selector {
-  override def select = _ => minLoadActor(collectedLoads)
+  override def select = msg => minLoadActor(collectLoads(msg))
 
   private def minLoadActor(loads: Seq[(ActorRef, Load)]): Option[ActorRef]
   = if (!loads.isEmpty) {
@@ -71,30 +74,80 @@ trait Collector {
 
   //保持しているactor,loadの組を返す
   // override it yourself!
-  def collectedLoads: Seq[(ActorRef, Float)]
+  def collectLoads: Any => Seq[(ActorRef, Float)]
 }
 
 trait PollingCollector extends Collector {
-  private[this] val loads = TransactionalMap[ActorRef, Float]()
+  protected val initialDelay: Long
+  protected val betweenPollingDelay: Long
+  protected val delayTimeUnit: TimeUnit
+  private[this] val loadMap = TransactionalMap[ActorRef, Float]()
 
-  override def collectedLoads = loads.toSeq
+  // Actor polling and update loads.
+  private val pollingActor = Actor.actorOf(new Actor {
+    protected def receive = {
+      case msg@RequestLoad
+      => if (loadMap.isEmpty) {
+        updateLoadsFirst
+      } else {
+        updateLoads
+      }
+    }
+  })
 
-  //TODO how to polling ??
+  override def collectLoads = _ => {
+    val loadSeq = atomic {
+      loadMap.toSeq
+    }
+    EventHandler.info(this, loadSeq)
+    loadSeq
+  }
+
+  // call this method when Actor's preStart()
+  def startPolling
+  = {
+    pollingActor.start
+    akka.actor.Scheduler.schedule(pollingActor, RequestLoad, initialDelay, betweenPollingDelay, delayTimeUnit)
+  }
+
+  //途中で集める時は一個ずつ集めるのをatomicに
+  private def updateLoads
+  = actors.foreach {
+    a => atomic {
+      (a ? RequestLoad).as[ReportLoad] match {
+        case Some(ReportLoad(load)) => loadMap +=(a, load)
+        case None =>
+      }
+    }
+  }
+
+  //最初だけは全部集め終わるのをatomicに
+  private def updateLoadsFirst
+  = atomic {
+    actors.foreach {
+      a => (a ? RequestLoad).as[ReportLoad] match {
+        case Some(ReportLoad(load)) => loadMap +=(a, load)
+        case None =>
+      }
+    }
+  }
+
 }
 
 trait OnDemandCollector extends Collector {
   private[this] val loads = TransactionalMap[ActorRef, Load]()
 
-  override def collectedLoads
-  = if (loads.isEmpty) {
-    updateLoadsFirst
-    atomic {
-      loads.toSeq
-    }
-  } else {
-    updateLoads
-    atomic {
-      loads.toSeq
+  override def collectLoads = _ => {
+    if (loads.isEmpty) {
+      updateLoadsFirst
+      atomic {
+        loads.toSeq
+      }
+    } else {
+      updateLoads
+      atomic {
+        loads.toSeq
+      }
     }
   }
 
--- src/main/scala/org/everpeace/akka/routing/package.scala
+++ src/main/scala/org/everpeace/akka/routing/package.scala
@@ -2,6 +2,7 @@ package org.everpeace.akka
 
 import akka.actor.{Actor, ActorRef}
 import scala.collection.JavaConversions._
+import java.util.concurrent.TimeUnit
 
 /**
  *
@@ -14,13 +15,20 @@ package object routing {
 
   // MinLoadSelectiveRouterのコンストラクタユーティリティ
   def minLoadSelectiveRouter(as: java.util.List[ActorRef]): ActorRef =
-    Actor.actorOf(new Actor with SelectiveRouter with MinLoadSelector with OnDemandCollector {
-      protected val actors = as toList
-    })
+    minLoadSelectiveRouter(as toList)
+
 
   // MinLoadSelectiveRouterのコンストラクタユーティリティ
-  def minLoadSelectiveRouter(as: Seq[ActorRef]): ActorRef =
-    Actor.actorOf(new Actor with SelectiveRouter with MinLoadSelector with OnDemandCollector {
-      protected val actors = as
-    })
+  def minLoadSelectiveRouter(as: Seq[ActorRef]): ActorRef
+  = Actor.actorOf(new Actor with SelectiveRouter with MinLoadSelector with PollingCollector {
+    val initialDelay = 3L
+    val betweenPollingDelay = 3L
+    val delayTimeUnit = TimeUnit.SECONDS
+    val actors = as
+
+    override def preStart() = {
+      startPolling
+    }
+  })
+
 }
